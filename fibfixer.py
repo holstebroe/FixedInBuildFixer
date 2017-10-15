d5d7f12e24caa76ca08c81fc5f0250283e58133e
Søren Holstebroe <soren@holstebroe.dk>
2017-10-15 10:57:54 +0200

    Added test for new change details support in pyteamcity.

--- fibfixer.py
+++ fibfixer.py
@@ -7,10 +7,15 @@ since_date_string = '20170101T000000+0000'
 builds = tc.builds.all().filter(
     build_type="Hotfix",
     since_date=since_date_string,
-    count=3)
+    count=30)
+
+print(builds.get(just_url=True))
+
 for build in builds:
     print(build.build_type_id + ": " + build.number + " - " + str(build.id))
     changes = tc.changes.all().filter(
-        count=5)
+        build=str(build.id))
+    print(changes.get(just_url=True))
     for change in changes:
-        print (change.username)
+        details = change.details
+        print(change.username + ": " + details.comment)
